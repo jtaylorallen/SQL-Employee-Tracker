01c30aa6b1d91c9692da041a8482a3b40cad0821
J. Taylor Allen <j.taylorallen@yahoo.com>
2022-01-30 19:46:31 -0600

    i hate this! so many typos for me

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+
+
+CREATE TABLE employees (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER NOT NULL,
+  admins_id INTEGER NOT NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+
+
+INSERT INTO employees 
+  (id, first_name, last_name, role_id, admins_id)
+VALUES
+  (1, 'Lori', 'Craig', 1, 1),
+  (2, 'Susan', 'Allen', 2, 2),
+  (3, 'Steven', 'Woodfin', 9, 0),
+  (4, 'Sally', 'Croft', 10, 0),
+  (5, 'Ruby', 'Davis', 11, 0),
+  (6, 'Molly', 'Clark', 3, 0),
+  (7, 'Tom', 'Walker', 4, 0),
+  (8, 'Hayden', 'Scott', 5, 0),
+  (9, 'Kate', 'King', 6, 0),
+  (10, 'Mike', 'Cannon', 7, 0),
+  (11, 'Julie', 'Field', 8, 0);
