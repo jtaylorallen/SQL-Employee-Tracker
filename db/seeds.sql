b0b46106ac07770dcf6da60c2559b265089bb646
J. Taylor Allen <j.taylorallen@yahoo.com>
2022-02-03 19:10:13 -0600

    lost track

--- db/seeds.sql
+++ db/seeds.sql
@@ -11,13 +11,13 @@ VALUES
   (2, 1, 'Assistant Principal', 89000),
   (3, 2, 'Kinder-Teacher', 55000),
   (4, 2, 'First-Teacher', 55000),
-  (5, 2, 'Second-Teacher', 55000);
-  (6, 2, 'Third-Teacher', 55000);
-  (7, 2, 'Forth-Taecher', 55000);
-  (8, 2, 'Fifth-Teacher', 55000);
+  (5, 2, 'Second-Teacher', 55000),
+  (6, 2, 'Third-Teacher', 55000),
+  (7, 2, 'Forth-Taecher', 55000),
+  (8, 2, 'Fifth-Teacher', 55000),
   (9, 3, 'Counselor', 35000),
   (10, 3, 'Nurse', 51000),
-  (11, 3, 'Secretary', 31000),
+  (11, 3, 'Secretary', 31000);
 
 
 INSERT INTO employees 
@@ -25,12 +25,12 @@ INSERT INTO employees
 VALUES
   (1, 'Lori', 'Craig', 1, 1),
   (2, 'Susan', 'Allen', 2, 2),
-  (3, 'Steven', 'Woodfin', 9, 0),
-  (4, 'Sally', 'Croft', 10, 0),
-  (5, 'Ruby', 'Davis', 11, 0),
-  (6, 'Molly', 'Clark', 3, 0),
-  (7, 'Tom', 'Walker', 4, 0),
-  (8, 'Hayden', 'Scott', 5, 0),
-  (9, 'Kate', 'King', 6, 0),
-  (10, 'Mike', 'Cannon', 7, 0),
-  (11, 'Julie', 'Field', 8, 0);
+  (3, 'Steven', 'Woodfin', 9, NULL),
+  (4, 'Sally', 'Croft', 10, NULL),
+  (5, 'Ruby', 'Davis', 11, NULL),
+  (6, 'Molly', 'Clark', 3, NULL),
+  (7, 'Tom', 'Walker', 4, NULL),
+  (8, 'Hayden', 'Scott', 5, NULL),
+  (9, 'Kate', 'King', 6, NULL),
+  (10, 'Mike', 'Cannon', 7, NULL),
+  (11, 'Julie', 'Field', 8, NULL);
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -13,6 +14,20 @@ app.get('/', (req, res) => {
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: process.env.ET_PW,
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
